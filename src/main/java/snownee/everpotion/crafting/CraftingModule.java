01a511b93a17757baf8c17f25ee262d21230c657
Snownee <1850986885@qq.com>
2020-10-17 17:44:10 +0800

    fix: potion core is not craftable

--- src/main/java/snownee/everpotion/crafting/CraftingModule.java
+++ src/main/java/snownee/everpotion/crafting/CraftingModule.java
@@ -4,11 +4,10 @@ import java.util.Optional;
 
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.IRecipeType;
-import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.event.AnvilUpdateEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.event.server.FMLServerStoppedEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import snownee.kiwi.AbstractModule;
+import snownee.kiwi.Kiwi;
 import snownee.kiwi.KiwiModule;
 import snownee.kiwi.Name;
 
@@ -23,23 +22,10 @@ public class CraftingModule extends AbstractModule {
     @Name("anvil")
     public static final IRecipeSerializer<EverAnvilRecipe> SERIALIZER = new EverAnvilRecipe.Serializer();
 
-    public static MinecraftServer SERVER;
-
-    @Override
-    protected void serverInit(FMLServerStartingEvent event) {
-        SERVER = event.getServer();
-    }
-
-    public void serverStopped(FMLServerStoppedEvent event) {
-        SERVER = null;
-    }
-
+    @SubscribeEvent
     public void onAnvilCrafting(AnvilUpdateEvent event) {
-        if (SERVER == null) {
-            return;
-        }
         AnvilContext ctx = new AnvilContext(event);
-        Optional<EverAnvilRecipe> result = SERVER.getRecipeManager().getRecipe(RECIPE_TYPE, ctx, null);
+        Optional<EverAnvilRecipe> result = Kiwi.getServer().getRecipeManager().getRecipe(RECIPE_TYPE, ctx, null);
         result.ifPresent(recipe -> {
             event.setOutput(recipe.getCraftingResult(ctx));
             event.setCost(ctx.cost);
