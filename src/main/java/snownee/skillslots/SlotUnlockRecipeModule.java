49bf78b97bdd3e675f842e979311d0e45c8a2256
Snownee <i@snownee.com>
2023-07-16 15:47:41 +0800

    split sources. full release

--- src/main/java/snownee/skillslots/SlotUnlockRecipeModule.java
+++
@@ -1,9 +0,0 @@
-package snownee.skillslots;
-
-import snownee.kiwi.AbstractModule;
-import snownee.kiwi.KiwiModule;
-
-@KiwiModule("slot_unlock_recipe")
-@KiwiModule.Optional
-public class SlotUnlockRecipeModule extends AbstractModule {
-}
--- src/main/java/snownee/skillslots/duck/SkillSlotsPlayer.java
+++
@@ -1,7 +0,0 @@
-package snownee.skillslots.duck;
-
-import snownee.skillslots.SkillSlotsHandler;
-
-public interface SkillSlotsPlayer {
-	SkillSlotsHandler skillslots$getHandler();
-}
